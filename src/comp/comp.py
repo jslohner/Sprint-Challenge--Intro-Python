d8159732b19f7fcfdcfcd975ed5dcbf5213479b3
Justin Lohner <lohner4@gmail.com>
2020-07-10 09:55:05 -0600

    got all of the oop problems done

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -4,24 +4,24 @@ import math
 import string
 
 class Human:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+	def __init__(self, name, age):
+		self.name = name
+		self.age = age
 
-    def __repr__(self):
-        return f"<Human: {self.name}, {self.age}>"
+	def __repr__(self):
+		return f"<Human: {self.name}, {self.age}>"
 
 humans = [
-    Human("Alice", 29),
-    Human("Bob", 32),
-    Human("Charlie", 37),
-    Human("Daphne", 30),
-    Human("Eve", 26),
-    Human("Frank", 18),
-    Human("Glenn", 42),
-    Human("Harrison", 12),
-    Human("Igon", 41),
-    Human("David", 31),
+	Human("Alice", 29),
+	Human("Bob", 32),
+	Human("Charlie", 37),
+	Human("Daphne", 30),
+	Human("Eve", 26),
+	Human("Frank", 18),
+	Human("Glenn", 42),
+	Human("Harrison", 12),
+	Human("Igon", 41),
+	Human("David", 31),
 ]
 
 # Write a list comprehension that creates a list of names of everyone
